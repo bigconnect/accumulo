c18c8bd2fed5dfff123314c9a27deba34132663f
Brian Loss <brianloss@apache.org>
2021-01-12 17:20:17 +0200

    Re-throw exception from LoggingRunnable (re #1808)
    * Re-throw the caught Throwable unless we received a more serious
      Error when attempting to log the original Throwable. In that case
      propagate the more serious error instead.

--- fate/src/main/java/org/apache/accumulo/fate/util/LoggingRunnable.java
+++ fate/src/main/java/org/apache/accumulo/fate/util/LoggingRunnable.java
@@ -21,33 +21,50 @@ import java.util.Date;
 import org.slf4j.Logger;
 
 public class LoggingRunnable implements Runnable {
-    private Runnable runnable;
-    private Logger log;
+  private Runnable runnable;
+  private Logger log;
 
-    public LoggingRunnable(Logger log, Runnable r) {
-        this.runnable = r;
-        this.log = log;
-    }
+  public LoggingRunnable(Logger log, Runnable r) {
+    this.runnable = r;
+    this.log = log;
+  }
 
-    @Override
-    public void run() {
+  @Override
+  public void run() {
+    try {
+      runnable.run();
+    } catch (Throwable t) {
+      boolean errorOnRun = (t instanceof Error);
+      try {
+        log.error("Thread \"{}\" died {}", Thread.currentThread().getName(), t.getMessage(), t);
+      } catch (Throwable t2) {
+        boolean errorOnLog = (t2 instanceof Error);
         try {
-            runnable.run();
-        } catch (Throwable t) {
-            try {
-                log.error("Thread \"{}\" died {}", Thread.currentThread().getName(), t.getMessage(), t);
-            } catch (Throwable t2) {
-                // maybe the logging system is screwed up OR there is a bug in the exception, like
-                // t.getMessage() throws a NPE
-                System.err.println(
-                        "ERROR " + new Date() + " Failed to log message about thread death " + t2.getMessage());
-                t2.printStackTrace();
+          // maybe the logging system is screwed up OR there is a bug in the exception, like
+          // t.getMessage() throws a NPE
+          System.err.println("ERROR " + new Date() + " Failed to log message about thread death "
+              + t2.getMessage());
+          t2.printStackTrace();
 
-                // try to print original exception
-                System.err.println("ERROR " + new Date() + " Exception that failed to log : " + t.getMessage());
-                t.printStackTrace();
-            }
+          // try to print original exception
+          System.err
+              .println("ERROR " + new Date() + " Exception that failed to log : " + t.getMessage());
+          t.printStackTrace();
+        } catch (Throwable t3) {
+          // If printing to System.err didn't work then don't try to log that exception but do
+          // re-throw it if it's the most serious failure we've seen so far.
+          boolean errorOnPrint = (t3 instanceof Error);
+          if (errorOnPrint && !errorOnLog && !errorOnRun)
+            throw t3;
         }
+
+        // If we got a more serious failure when attempting to log the message,
+        // then throw that instead of the original exception.
+        if (errorOnLog && !errorOnRun)
+          throw t2;
+      }
+      throw t;
     }
+  }
 
 }
